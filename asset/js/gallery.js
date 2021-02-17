e6792b7ea28dce5ffc6bbc7809a4d60abd1b3432
Elshadai <elshadaikassutegegn@gmail.com>
2021-02-17 22:29:12 +0300

    Gallery finished

--- asset/js/gallery.js
+++ asset/js/gallery.js
@@ -1,31 +1,47 @@
-const image = document.getElementsByClassName('carousel-item');
+const container = document.getElementById('container');
+
 document.addEventListener("DOMContentLoaded", () => {
-    //load_fromPlaceHolder();
-    setInterval(
-        loadDataNew, 6000
-    );
-});
-async function load_fromPlaceHolder_new() {
+    display_image();
+})
+
+
+
+async function load_image() {
 
     //open the request 
-    let response = await fetch('https://picsum.photos/seed/picsum/200/300');
-
-    let data = await response.url;
+    let response = await fetch("https://picsum.photos/v2/list?page=2&limit=100");
+    console.log(response);
+    let data = await response.json();
 
     return data;
 
 }
 
-function loadDataNew() {
-    load_fromPlaceHolder_new().then(function(posts) {
-            //iterate over each post [100 posts]
+async function display_image() {
+
+    load_image().then(function(images) {
             let output = '';
-            console.log(posts)
-            output += `<img src=${posts}>`;
-            image[0].innerHTML = output;
+
+            images.forEach(image => {
+                let imgUrl = image.download_url.slice(0, image.download_url.length - 9);
+                imgUrl += "400/400"
+                output += `<div class="four wide column">
+                        <div class="item">
+                            <div class="images">
+                                <img src="${imgUrl}" alt="">
+                            </div>
+                            <div class="content">
+                                <div class="header author">${image.author}</div>
+                            </div>
+                        </div>
+                    </div>`
+            });
+            container.innerHTML += output;
+
         })
         .catch(function(err) {
-            console.log(err);
+            // console.log(err);
         });
+
 
 }
